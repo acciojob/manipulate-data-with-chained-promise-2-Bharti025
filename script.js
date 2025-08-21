a77592a2021c4c682d3b39c779ed31522630a223
Bharti <110337668+Bharti025@users.noreply.github.com>
2025-08-21 17:18:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
 //your JS code here. If required.
+
+let output=document.getElementById("output");
+let arr = [1, 2, 3, 4]; // Define the array
+
+const promise1=new Promise((resolve,reject)=>{
+	setTimeout(()=>{
+		let ans=arr.filter((element,index)=>
+			 element % 2 === 0);
+		resolve(ans);
+	},1000);
+})
+
+const promise2= new Promise((resolve,reject)=>{
+setTimeout(()=>{
+		let result=ans.map((element,index)=>{
+			element*2;
+		})
+		resolve(result);
+	},2000);
+	
+});
+
+promise1.then((data)=>{
+output.textContent(data);	
+	return promise2;
+}).then((data)=>{
+	output.textContent(data);	
+})
+
+
+
